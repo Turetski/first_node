4bdba9dc44d7048ba93b566dc890205d5613f8ff
Kursant <Kursant>
2017-04-08 12:23:52 +0700

    11

---
+++ echo.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var url = require('url');
+var server = new http.Server(function(req,res){
+	console.log(req.method, req.url);
+	var urlParser = url.parse(req.url, true);
+	console.log(urlParser);
+    if(urlParser.pathname == '/SayMeow' && urlParser.query.message){
+    	res.setHeader('Cache-control','no-cache');
+    	res.writeHead(200,"OK",{'Cache-control':'no-cache'});
+
+    	res.end('Cat says: '+urlParser.query.message)
+    } else {
+    	res.statusCode = 404;
+    	res.end("Page not found")
+    }
+	res.end('404');
+});
+server.listen(1338, '127.0.0.1');
