2895009517a170da3236c56e169dd152aa0e5d2c
Kursant <Kursant>
2017-04-08 15:20:13 +0700

    pizza hut

---
+++ ionic/pizza/www/js/ionic.cloud.init.js
@@ -0,0 +1,8 @@
+angular.module('ionic.cloud.init',['ionic.cloud'])
+  .config(function($ionicCloudProvider)) {
+  	$ionicCloudProvider.init({
+  		"core":{
+  			"app_id: "aeb96e53225a"
+  		}
+  	});
+  }
