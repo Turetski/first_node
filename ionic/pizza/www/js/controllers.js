bf0fb0b2db43cae8d7492dcc4a84cda8e630a774
Kursant <Kursant>
2017-04-08 15:56:23 +0700

    wow wow

--- ionic/pizza/www/js/controllers.js
+++ ionic/pizza/www/js/controllers.js
@@ -1,11 +1,28 @@
 angular.module('app.controllers', [])
   
-.controller('page2Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('page2Ctrl', ['$scope', '$stateParams', '$ionicUser', '$ionicAuth', '$state' // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $stateParams, $ionicUser, $ionicAuth, $state) {
+  $scope.data = {
+  	'email': '',
+  	'password': ''
+  }
+  
 
+  if($ionicAuth.isAuthenticated()){
+  	$ionicUser.load().then(function(){});
+  	$scope.success = 'Allready authenticated';
+  }
 
+  $scope.login = function(){
+  	$scope.error ='';
+  	$ionicAuth.login('basic', $scope.data)).then(function(){
+        $scope.success = "It is all OK";
+      },function(){
+        $scope.error = "It is error";
+      })
+  }
 }])
    
 .controller('signupCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
--- ionic/pizza/www/js/ionic.cloud.init.js
+++ ionic/pizza/www/js/ionic.cloud.init.js
@@ -2,7 +2,7 @@ angular.module('ionic.cloud.init',['ionic.cloud'])
   .config(function($ionicCloudProvider)) {
   	$ionicCloudProvider.init({
   		"core":{
-  			"app_id: "aeb96e53225a"
+  			"app_id: "5032027d"
   		}
   	});
   }
